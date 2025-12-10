ed48cd622d84562c70e2163cf0a90d691a524b4d
ChloeMVan <74740805+ChloeMVan@users.noreply.github.com>
2025-12-09 19:44:16 -0600

    changed file type

--- inference.py
+++ inference.py
@@ -162,7 +162,7 @@ def main():
 
     # latency log
         # path for logging end-to-end generation latency (TeaCache-style)
-    latency_log_path = os.path.join(save_dir, "latency.csv")
+    latency_log_path = os.path.join(save_dir, "latency.txt")
 
 
     # == Iter over all samples ==

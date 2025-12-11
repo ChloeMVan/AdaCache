31eb23610eda28a04fb6e8b9e6c467add662997b
root <cvancaemelbecke@gmail.com>
2025-12-11 08:09:40 +0000

    m

--- run.sh
+++ run.sh
@@ -5,4 +5,6 @@ bash run_sample_image.sh configs/sample.py
 sleep 5s
 python clean.py
 sleep 5s
-bash run_sample_video.sh configs/sample_adacache_moreg.py
+bash run_sample_video.sh configs/sample_adacache.py
+
+

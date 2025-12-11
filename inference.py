470a8f3e47e9223922e22ed228d004ab1f120fe0
ChloeMVan <74740805+ChloeMVan@users.noreply.github.com>
2025-12-10 20:57:51 -0600

    reduced sample

--- inference.py
+++ inference.py
@@ -157,7 +157,7 @@ def main():
     multi_resolution = cfg.get("multi_resolution", None)
     batch_size = 1
     #num_sample = cfg.get("num_sample", 2)
-    num_sample = 2
+    num_sample = 1
     loop = cfg.get("loop", 1)
     condition_frame_length = cfg.get("condition_frame_length", 5)
     condition_frame_edit = cfg.get("condition_frame_edit", 0.0)
